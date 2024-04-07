e6ba04df0e544aec14068d6559fdc0868917dc52
026024 <120216635+Goncalo04Costa@users.noreply.github.com>
2024-04-07 11:50:39 +0100

    controllers

--- WebApplication1/Controllers/EquipamentosController.cs
+++ WebApplication1/Controllers/EquipamentosController.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Modelos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace WebApplication1.Controllers
 {
--- WebApplication1/Controllers/HospitalController.cs
+++ WebApplication1/Controllers/HospitalController.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Modelos;
 using Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApplication1.Controllers
 {
