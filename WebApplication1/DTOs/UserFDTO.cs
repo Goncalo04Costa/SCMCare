561d6aeb625075bf0ab6573a55a909eee6bf7e78
026024 <120216635+Goncalo04Costa@users.noreply.github.com>
2024-04-28 17:48:24 +0100

    logins

---
+++ WebApplication1/DTOs/UserFDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.DTOs
+{
+    public class UserFDTO
+    {
+       public int ID { get; set; }
+
+        [Required(ErrorMessage = " é obrigatória.")]
+        [MaxLength(250, ErrorMessage = "O email deve ter, no maximo, 250 caracteres.")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "A password é obrigatória.")]
+        [MaxLength(100, ErrorMessage = "A password deve ter, no maximo, 100 caracteres.")]
+        [MinLength(8, ErrorMessage = "A password deve ter, no minimo, 8 caracteres.")]
+        [NotMapped]
+        public string PassWord { get; set; }
+    }
+}
