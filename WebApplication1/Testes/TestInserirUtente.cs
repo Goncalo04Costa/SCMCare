861a32b1e6dc9869d3346dc309654cfe124306d1
Danielabrito2003 <a25591@alunos.ipca.pt>
2024-04-29 11:22:40 +0100

    .

---
+++ WebApplication1/Testes/TestInserirUtente.cs
@@ -0,0 +1,60 @@
+﻿using Nunit.Framework;
+using WebApplication1.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Modelos;
+using WebApplication1;
+using Microsoft.AspNetCore.Routing;
+using System.Data.Entity.Core.Objects;
+
+
+namespace WebApplication1.Testes
+{
+
+    [TestFixture]
+    public class UtentesControllerTests
+    {
+        private UtentesController _controller;
+        private Mock<AppDbContext> _mockDbContext;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockDbContext = new Mock<AppDbContext>();
+            _controller = new UtentesController(_mockDbContext.Object);
+        }
+
+        [Test]
+        public async Task InserirUtente_WithValidUtente_ReturnsOk()
+        {
+            // Arrange
+            var utente = new Utente
+            {
+                Id = 1,
+                Nome = "Teste",
+
+            };
+
+            // Act
+            var result = await _controller.InserirUtente(utente) as OKObjectResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Utente adicionado com sucesso", result.Value);
+        }
+
+        [Test]
+        public async Task InserirUtente_WithNullUtente_ReturnsBadRequest()
+        {
+            // Arrange
+            Utente utente = null;
+
+            // Act 
+            var result = await _controller.InserirUtente(utente) as BadRequestObjectResult;
+
+            // Assert 
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Objeto inválido", result.Value);
+        }
+    }
+}
