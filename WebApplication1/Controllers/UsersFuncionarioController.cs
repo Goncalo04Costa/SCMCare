b10972dffeb049bb87ff49148bef2e52bfe73771
026024 <120216635+Goncalo04Costa@users.noreply.github.com>
2024-05-01 00:18:35 +0100

    logins

--- WebApplication1/Controllers/UsersFuncionarioController.cs
+++ WebApplication1/Controllers/UsersFuncionarioController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Modelos;
 using System;
@@ -13,11 +14,11 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class UserFuncionarioController : ControllerBase
     {
-        private readonly AppDbContext _context;
+        private readonly UserManager<UserFuncionario> _userManager;
 
-        public UserFuncionarioController(AppDbContext context)
+        public UserFuncionarioController(UserManager<UserFuncionario> userManager)
         {
-            _context = context;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -25,7 +26,7 @@ namespace WebApplication1.Controllers
             int? idMin = null, int? idMax = null,
             string nomeMin = null, string nomeMax = null)
         {
-            IQueryable<UserFuncionario> query = _context.UserFuncionario;
+            IQueryable<UserFuncionario> query = _userManager.Users;
 
             if (idMin.HasValue)
             {
@@ -39,16 +40,16 @@ namespace WebApplication1.Controllers
 
             if (!string.IsNullOrEmpty(nomeMin) && !string.IsNullOrEmpty(nomeMax))
             {
-                query = query.Where(d => string.Compare(d.User, nomeMin, StringComparison.OrdinalIgnoreCase) >= 0 &&
-                                          string.Compare(d.User, nomeMax, StringComparison.OrdinalIgnoreCase) <= 0);
+                query = query.Where(d => string.Compare(d.UserName, nomeMin, StringComparison.OrdinalIgnoreCase) >= 0 &&
+                                          string.Compare(d.UserName, nomeMax, StringComparison.OrdinalIgnoreCase) <= 0);
             }
             else if (!string.IsNullOrEmpty(nomeMin))
             {
-                query = query.Where(d => string.Compare(d.User, nomeMin, StringComparison.OrdinalIgnoreCase) >= 0);
+                query = query.Where(d => string.Compare(d.UserName, nomeMin, StringComparison.OrdinalIgnoreCase) >= 0);
             }
             else if (!string.IsNullOrEmpty(nomeMax))
             {
-                query = query.Where(d => string.Compare(d.User, nomeMax, StringComparison.OrdinalIgnoreCase) <= 0);
+                query = query.Where(d => string.Compare(d.UserName, nomeMax, StringComparison.OrdinalIgnoreCase) <= 0);
             }
 
             var dados = await query.ToListAsync();
@@ -56,70 +57,15 @@ namespace WebApplication1.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<UserFuncionario>> ObterUserFuncionario(int id)
+        public async Task<ActionResult<UserFuncionario>> ObterUserFuncionario(string id)
         {
-            var userFuncionario = await _context.UserFuncionario.FirstOrDefaultAsync(d => d.FuncionarioId == id);
+            var userFuncionario = await _userManager.FindByIdAsync(id);
             if (userFuncionario == null)
             {
                 return NotFound();
             }
             return Ok(userFuncionario);
         }
-
-        [HttpPost]
-        public async Task<ActionResult<UserFuncionario>> InserirUserFuncionario([FromBody] UserFuncionario user)
-        {
-            if (user == null || !ModelState.IsValid)
-            {
-                return BadRequest("Objeto inválido");
-            }
-
-            _context.UserFuncionario.Add(user);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(ObterUserFuncionario), new { id = user.FuncionarioId }, user);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> AtualizarUserFuncionario(int id, [FromBody] UserFuncionario novoUser)
-        {
-            var user = await _context.UserFuncionario.FirstOrDefaultAsync(d => d.FuncionarioId == id);
-            if (user == null)
-            {
-                return NotFound($"Não foi possível encontrar o funcionário com o ID {id}");
-            }
-
-            user.User = novoUser.User;
-            user.Passe = novoUser.Passe; // Suponho que também precise atualizar a senha
-
-            try
-            {
-                await _context.SaveChangesAsync();
-
-                return Ok($"Foi atualizado o funcionário com o ID {id}");
-            }
-            catch (Exception e)
-            {
-                return StatusCode(500, $"Erro ao atualizar o funcionário: {e.Message}");
-            }
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> RemoverUserFuncionario(int id)
-        {
-            var user = await _context.UserFuncionario.FirstOrDefaultAsync(d => d.FuncionarioId == id);
-            if (user == null)
-            {
-                return NotFound($"Não foi possível encontrar o funcionário com o ID {id}");
-            }
-
-            _context.UserFuncionario.Remove(user);
-            await _context.SaveChangesAsync();
-
-            return Ok($"Foi removido o funcionário com o ID {id}");
-        }
-
-
         [HttpPost("registrar")]
         public async Task<IActionResult> RegistrarUserFuncionario([FromBody] UserFuncionarioRegistroDto userDto)
         {
@@ -129,29 +75,62 @@ namespace WebApplication1.Controllers
             }
 
             // Verifique se o usuário já existe
-            var existingUser = await _context.UserFuncionario.FirstOrDefaultAsync(u => u.User == userDto.User);
+            var existingUser = await _userManager.FindByNameAsync(userDto.User);
             if (existingUser != null)
             {
                 return Conflict("Usuário já existe");
             }
 
             // Crie um novo objeto UserFuncionario com base nos dados recebidos
-            var newUser = new UserFuncionario
-            {
-                User = userDto.User,
-                Passe = userDto.Passe,
-                // Outras propriedades, se houver
-            };
+            var newUser = new UserFuncionario { UserName = userDto.User };
 
             // Adicione o novo usuário ao contexto do banco de dados e salve as alterações
-            _context.UserFuncionario.Add(newUser);
-            await _context.SaveChangesAsync();
+            var result = await _userManager.CreateAsync(newUser, userDto.Passe);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
 
             // Retorne uma resposta de sucesso com os detalhes do novo usuário
-            return CreatedAtAction(nameof(ObterUserFuncionario), new { id = newUser.FuncionarioId }, newUser);
+            return CreatedAtAction(nameof(ObterUserFuncionario), new { id = newUser.Id }, newUser);
         }
 
 
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AtualizarUserFuncionario(string id, [FromBody] UserFuncionarioRegistroDto userDto)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound($"Não foi possível encontrar o funcionário com o ID {id}");
+            }
 
+            user.UserName = userDto.User;
+            var result = await _userManager.UpdateAsync(user);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok($"Foi atualizado o funcionário com o ID {id}");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoverUserFuncionario(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound($"Não foi possível encontrar o funcionário com o ID {id}");
+            }
+
+            var result = await _userManager.DeleteAsync(user);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok($"Foi removido o funcionário com o ID {id}");
+        }
     }
 }
--- WebApplication1/Modelos/UserFuncionario.cs
+++ WebApplication1/Modelos/UserFuncionario.cs
@@ -3,15 +3,8 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Modelos
 {
-    public class UserFuncionario  : IdentityUser
+    public class UserFuncionario : IdentityUser
     {
-        [Required]
         public int FuncionarioId { get; set; }
-
-        [Required]
-        public string User { get; set; }
-
-        [Required]
-        public string Passe { get; set; }
     }
 }
