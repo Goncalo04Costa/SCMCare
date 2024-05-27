4d286592b7aabab3c106e3e5363d6882f5f40c85
026024 <120216635+Goncalo04Costa@users.noreply.github.com>
2024-05-27 18:59:07 +0100

    logins

--- WebApplication1/startup.cs
+++ WebApplication1/startup.cs
@@ -21,7 +21,7 @@ namespace WebApplication1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("LigacaoSofia")));
+                options.UseSqlServer(Configuration.GetConnectionString("LigacaoGoncalo")));
 
             // Add Identity services
             services.AddIdentity<IdentityUser, IdentityRole>()
